c954fc4c64124acd8036b6f25fba0dcbbf7b204f
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-22 14:28:44 +0300

    Calling functions inside a component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,16 @@
 import {Bar1, Bar2} from "./components/bars";
 
 function App() {
+  let x = aCat();
   return (
     <div>
       <h1>Trying out warevaaaaa, oh react it is</h1>
       <MyAge></MyAge>
       <Bar1></Bar1>
       <Bar2/>
+      {x}
+      <br></br>
+      {aCat()}
     </div>
   );
 }
@@ -18,6 +22,10 @@ function MyAge(){
       <p style={{color:"red"}}>My age is 23</p>
     </div>
   )
+}
+
+function aCat(){
+  return 23
 }
 
 export default App;
