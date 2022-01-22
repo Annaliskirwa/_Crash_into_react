4e4bb95a382fdbffefceafced34ab362ca2ff332
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-22 13:55:30 +0300

    React functions and components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,9 +1,19 @@
 function App() {
   return (
     <div>
-      <h1>TRying out warevaaaaa</h1>
+      <h1>Trying out warevaaaaa, oh react it is</h1>
+      <MyAge></MyAge>
     </div>
   );
+}
+
+function MyAge(){
+  return(
+    <div>
+      <h2>Starting out on components</h2>
+      <p>My age is 23</p>
+    </div>
+  )
 }
 
 export default App;
