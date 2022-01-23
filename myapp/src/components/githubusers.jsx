833e1fe014c289afab553d14446bc6bb42dae787
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-23 21:15:59 +0300

    Getting data from backend api using axios

--- myapp/src/components/githubusers.jsx
+++ myapp/src/components/githubusers.jsx
@@ -1,4 +1,20 @@
+import React,{useState, useEffect} from "react";
+import axios from "axios";
+
 const AllUsers = ()=>{
+    const [users, setUsers] = useState([]);
+
+    useEffect(()=> {
+        const fetchData=async()=>{
+            let data =  await axios({
+                method: "get",
+                url: "https://api.github.com/users"
+            });
+            setUsers(data);
+            console.log(data)
+        };
+        fetchData();
+    },[]);
     return(
         <div>
             <p>This is a list of all github users</p>
