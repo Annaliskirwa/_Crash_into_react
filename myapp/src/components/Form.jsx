0d1938e72a5489d0fb22d68f555d7003002ce84b
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-23 18:55:50 +0300

    Adding submit to the  form

--- myapp/src/components/Form.jsx
+++ myapp/src/components/Form.jsx
@@ -9,6 +9,21 @@ const Myform = ()=>{
     return (
         <div>
             <h1>My cool form</h1>
+            <div style={{margin:"auto", width:"50%"}}>
+            <div className = "w3-card-3 w3-padding">
+                <p>
+                    <label>Name</label>
+                    <input type = "text" className="w3-input w3-border"></input>
+                </p>
+                <p>
+                    <label>Age</label>
+                    <input type = "text" className="w3-input w3-border"></input>
+                </p>
+            </div>
+            <div className = "w3-margin-top w3-center">
+                <button className="w3-button w3-round w3-green w3-ripple ">Submit</button>
+            </div>
+            </div>
         </div>
     )
 }
