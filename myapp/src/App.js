7da3321f450dbc18fec0d78e65a41077bb736926
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-23 16:42:24 +0300

    useEffect hook, page rendering log details

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,13 @@
 import {Bar1, Bar2, MyCatsAge} from "./components/bars";
 import StateButton from "./components/StateButton";
+import EffectComp from "./components/EffectComp";
 
 function App() {
   let x = catsAge(23)
   return (
     <div>
-     <StateButton/>
+     {/* <StateButton/> */}
+     <EffectComp/>
     </div>
   );
 }
--- myapp/src/components/EffectComp.jsx
+++ myapp/src/components/EffectComp.jsx
@@ -1,14 +1,47 @@
-import React,{useEffect} from "react";
+import React,{useEffect, useState} from "react";
 
 const EffectComp = ()=>{
+    const [age, setAge]=useState(0);
+    const [cat, setCat]=useState({age:23, name: "Kelly"});
 
-    useEffect(()=>{}, []);
+    function increment(){
+        setCat({...cat, age: cat.age + 1});
+        setAge(age+1);
+    }
+
+    function decrement(){
+        setCat({...cat,age:cat.age - 1});
+        setAge(age-1);}
+
+    useEffect(()=>{
+        console.log("I have rendered")
+    }, []);
 
     return (
         <div>
-            <h1>Hello from use effect</h1>
+             <button onClick={()=>increment()} className = "w3-button w3-ripple w3-round w3-margin-left w3-margin-right w3-blue">
+                Increment
+            </button>
+            <span className = "w3-xxlarge w3-margin-left w3-margin-right">{age}</span>
+            <button onClick={()=>decrement()} className = "w3-button w3-ripple w3-round w3-margin-left w3-margin-right w3-red">
+                Decrement
+            </button>
+            <StateButton age={age} cat={cat}/>
         </div>
     )
 }
+
+const StateButton = ({cat})=>{
+
+
+    return(
+        <div>
+            <div>
+                <h1>Cat Name:{cat.name}</h1>
+                <h2>Cats Age:{cat.age}</h2>
+            </div>
+        </div>
+    );
+};
 
 export default EffectComp;
