1fc851c37dfeea7fa63f8dfd32f0aab85d8171a7
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-23 13:37:30 +0300

    Copying values into a variable

--- myapp/src/components/StateButton.jsx
+++ myapp/src/components/StateButton.jsx
@@ -1,9 +1,11 @@
 import React, {useState} from "react"
 
 const StateButton = ()=>{
-    const [age, setAge]=useState(0)
+    const [age, setAge]=useState(0);
+    const [cat, setCat]=useState({age:23, name: "Kelly"});
 
     function increment(){
+        setCat({...cat, age: cat.age + 1});
         setAge(age+1);
     }
 
@@ -21,6 +23,10 @@ const StateButton = ()=>{
             <button onClick={()=>decrement()} className = "w3-button w3-ripple w3-round w3-margin-left w3-margin-right w3-red">
                 Decrement
             </button>
+            <div>
+                <h1>Cat Name:{cat.name}</h1>
+                <h2>Cats Age:{cat.age}</h2>
+            </div>
         </div>
     );
 };
