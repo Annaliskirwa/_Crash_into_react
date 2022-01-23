66053c343e99b0b7a64a51875cd5ab315f085108
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-23 13:58:24 +0300

    UseEffect component

---
+++ myapp/src/components/EffectComp.jsx
@@ -0,0 +1,14 @@
+import React,{useEffect} from "react";
+
+const EffectComp = ()=>{
+
+    useEffect(()=>{}, []);
+
+    return (
+        <div>
+            <h1>Hello from use effect</h1>
+        </div>
+    )
+}
+
+export default EffectComp;
