80204bec1342f1818b6cd68f610609c73d13a558
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-23 18:15:40 +0300

    changing color of button to green when age is 5

--- myapp/src/components/EffectComp.jsx
+++ myapp/src/components/EffectComp.jsx
@@ -3,6 +3,7 @@ import React,{useEffect, useState} from "react";
 const EffectComp = ()=>{
     const [age, setAge]=useState(0);
     const [cat, setCat]=useState({age:23, name: "Kelly"});
+    const [bColor, setbColor] = useState("w3-blue");
 
     function increment(){
         setCat({...cat, age: cat.age + 1});
@@ -14,19 +15,22 @@ const EffectComp = ()=>{
         setAge(age-1);}
 
     useEffect(()=>{
-        console.log("I have rendered")
+        // console.log("I have rendered")
+        if (age > 5){
+            setbColor("w3-green");
+            console.log("I am to change the color to green")
+        }
     }, [age]);
 
     return (
         <div>
-             <button onClick={()=>increment()} className = "w3-button w3-ripple w3-round w3-margin-left w3-margin-right w3-blue">
-                Increment
-            </button>
-            <span className = "w3-xxlarge w3-margin-left w3-margin-right">{age}</span>
-            <button onClick={()=>decrement()} className = "w3-button w3-ripple w3-round w3-margin-left w3-margin-right w3-red">
+        <button onClick={() => increment()} className={'w3-button w3-ripple w3-round w3-margin-left w3-margin-right w3-blue ${bColor}'}>
+            Increment
+        </button>
+        <span className="w3-xxlarge w3-margin-left w3-margin-right">{age}</span>
+        <button onClick={() => decrement()} className="w3-button w3-ripple w3-round w3-margin-left w3-margin-right w3-red">
                 Decrement
-            </button>
-            <StateButton age={age} cat={cat}/>
+            </button><StateButton age={age} cat={cat} />
         </div>
     )
 }
